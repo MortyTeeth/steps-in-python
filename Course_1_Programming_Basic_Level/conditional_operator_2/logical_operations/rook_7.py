d50ceb67efee9403961f903fba4067a1e4194c57
Rubick <andrew.fox.3.14@gmail.com>
2025-06-03 09:54:41 +0300

    Project update: logical operations

---
+++ Course_1_Programming_Basic_Level/conditional_operator_2/logical_operations/rook_7.py
@@ -0,0 +1,9 @@
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+
+if ((a >= 1 and a <= 8) and (b == d)) or ((b >= 1 and b <=8) and (a == c)):
+    print('YES')
+else:
+    print('NO')
---
+++ Course_1_Programming_Basic_Level/conditional_operator_2/logical_operations/the_course_of_the_king_8.py
@@ -0,0 +1,9 @@
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+
+if ((-1 <= (c - a) <= 1) and ((-1 <= (d - b) <= 1))):
+    print('YES')
+else:
+    print('NO')
